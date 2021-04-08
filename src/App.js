512b1d751d7d4edd26d55938829845d95d92ac5a
Hunter Redding <hunterred50@gmail.com>
2021-04-08 11:21:33 -0400

    mobile bkgd fix attempt

--- src/App.js
+++ src/App.js
@@ -152,18 +152,18 @@ function App() {
         <h1 style={{margin: "0px", fontFamily: "Oswald, sans-serif", fontWeight: "400", paddingBottom: "0em"}}>Photos</h1>
           <div style={{display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
             <img style={{width: "50%", padding: "2em 1em"}} alt="magazine" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/8UG0oH5bSSAqGXA5ljrQ_magazine.jpg"/>
-            <h3 style={{padding: "0 1em", fontSize: "2.5vw", fontWeight: "lighter"}}>Steve and Dawn on the cover of LAKE Destination Lanier, Home Living in North Georgia</h3>
+            <h3 style={{padding: "0 1em", fontSize: "2.5vw", fontWeight: "400"}}>Steve and Dawn on the cover of LAKE Destination Lanier, Home Living in North Georgia</h3>
           </div>
           <div style={{display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
-            <h3 style={{padding: "0 1em", fontSize: "2.5vw", fontWeight: "lighter"}}>Steve and Dawn with How Money Works co-author, Tom Matthews, on Leadership Live interviewing Bob Proctor</h3>
+            <h3 style={{padding: "0 1em", fontSize: "2.5vw", fontWeight: "400"}}>Steve and Dawn with How Money Works co-author, Tom Matthews, on Leadership Live interviewing Bob Proctor</h3>
             <img style={{width: "50%", padding: "2em 1em"}} alt="magazine" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/cH3ZNlmRqmzGsVLa9If5_wealthwave.jpg"/>
           </div>
           <div style={{display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
             <img style={{width: "40%", padding: "2em 1em"}} alt="magazine" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/9oAbt199QECuqkAEhgV0_marriage.jpg"/>
-            <h3 style={{padding: "0 1em", fontSize: "2.5vw", fontWeight: "lighter"}}>A wedding portrait of Steve and Dawn</h3>
+            <h3 style={{padding: "0 1em", fontSize: "2.5vw", fontWeight: "400"}}>A wedding portrait of Steve and Dawn</h3>
           </div>
           <div style={{display: "flex", justifyContent: "space-evenly", alignItems: "center"}}>
-            <h3 style={{padding: "0 1em", fontSize: "2.5vw", fontWeight: "lighter"}}>A picture of Dawn outside of Bona Allen Mansion, the new homebase of Siebold Success Network</h3>
+            <h3 style={{padding: "0 1em", fontSize: "2.5vw", fontWeight: "400"}}>A picture of Dawn outside of Bona Allen Mansion, the new homebase of Siebold Success Network</h3>
             <img style={{width: "40%", padding: "2em 1em"}} alt="magazine" src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/3210473/settings_images/CecHWLjTViZjXMgaHqOw_mansion.jpg"/>
           </div>
         </Element>
