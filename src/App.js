5d823def80ff6122016cd0de30146506f6fec2ec
Hunter Redding <hunterred50@gmail.com>
2022-05-12 10:53:16 -0400

    height fix

--- src/App.js
+++ src/App.js
@@ -161,8 +161,8 @@ function App() {
         </div>
       </div>
       <div style={{scrollBehavior: "smooth"}}> {/* adjust height in next two lines */}
-      <Element name="episodes" style={{backgroundColor: "rgba(35, 38, 50)", marginTop: "-6em", display: "flex", justifyContent: "center", height: "145em"}}>
-        <div style={{backgroundColor: "#ffffff", paddingTop: "10px", width: "50em", height: "140em", color: "black", zIndex: "2"}}>
+      <Element name="episodes" style={{backgroundColor: "rgba(35, 38, 50)", marginTop: "-6em", display: "flex", justifyContent: "center", height: "150em"}}>
+        <div style={{backgroundColor: "#ffffff", paddingTop: "10px", width: "50em", height: "145em", color: "black", zIndex: "2"}}>
         <h1 style={{fontFamily: "Oswald, sans-serif", fontWeight: "400"}}>Watch the show:</h1>
           <div id="video" style={{width: "90%", margin: "auto"}}>
             <div className="wistia_responsive_padding" style={{padding: "56.25% 0 0 0", position: "relative"}}><div className="wistia_responsive_wrapper" style={{height: "100%", left: "0", position: "absolute", top: "0", width: "100%"}}>
